a4658223e56934116cf8d0563d48f79c5b2c7918
hassania-elfalah <elfalahhassania@gmail.com>
2026-05-21 03:49:18 +0200

    Add /migrate helper route for easy database setup on Vercel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/migrate', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        return '<pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return 'Error migrating: ' . $e->getMessage();
+    }
+});
